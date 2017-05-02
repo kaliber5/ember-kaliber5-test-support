d0937f01aebe3465c77a8dab6c97eeeb99884486
simonihmig <ihmig@kaliber5.de>
2017-05-02 22:39:45 +0200

    Fix default test selector for Ember 2.13

--- vendor/add-default-test-selector.js
+++ vendor/add-default-test-selector.js
@@ -3,8 +3,11 @@
 (function() {
   Ember.Component.reopen({
     init: function() {
-      if (this.get('tagName') !== '' && Ember.isEmpty(this.get('data-test-selector'))) {
-        this.set('data-test-selector', Object.getPrototypeOf(this)._debugContainerKey.replace(/^component:/, ''));
+      if (this.get('tagName') !== ''
+        && Ember.isEmpty(this.get('data-test-selector'))
+        && typeof this._debugContainerKey === 'string'
+      ) {
+        this.set('data-test-selector', this._debugContainerKey.replace(/^component:/, ''));
       }
       // make sure we call _super afterwards, so the init of ember-test-selectors can see our change
       // see https://github.com/simplabs/ember-test-selectors/blob/master/vendor/ember-test-selectors/patch-component.js
