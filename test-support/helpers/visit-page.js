aead7b934d7b77d7b9c0600c00056b1422ca3f30
simonihmig <ihmig@kaliber5.de>
2017-11-10 19:39:29 +0100

    Allow URL params for visit-page helper

--- test-support/helpers/visit-page.js
+++ test-support/helpers/visit-page.js
@@ -8,15 +8,16 @@ const { warn } = Ember;
  * Visit the given page. Fetches the URL from the page object if available
  *
  * @param page
+ * @param params
  * @returns {RSVP.Promise}
  * @public
  */
-export default function visitPage(page) {
+export default function visitPage(page, params) {
   let pageObject = getPageObject(page);
 
   if (pageObject) {
     // if there is a page object, then use that visit() method to visit the page
-    return pageObject.visit();
+    return pageObject.visit(params);
   } else {
     // else construct the route name and visit it
     warn(`You visited the ${page} page directly in your tests, but you should create a page object for it!`, false, { id: 'ember-kaliber5-test-support.pageObject' });
