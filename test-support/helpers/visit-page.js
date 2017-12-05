b418e3c03c1bc57ce144cd3dc9e53f40ffeb69e7
simon <ihmig@kaliber5.de>
2017-12-05 17:15:56 +0100

    Possible fix for async timing issues in visitPage (weird)

--- test-support/helpers/visit-page.js
+++ test-support/helpers/visit-page.js
@@ -11,16 +11,16 @@ import { visit } from 'ember-native-dom-helpers';
  * @returns {RSVP.Promise}
  * @public
  */
-export default function visitPage(page, params) {
+export default async function visitPage(page, params) {
   let pageObject = getPageObject(page);
 
   if (pageObject) {
     // if there is a page object, then use that visit() method to visit the page
-    return pageObject.visit(params);
+    await pageObject.visit(params);
   } else {
     // else construct the route name and visit it
     warn(`You visited the ${page} page directly in your tests, but you should create a page object for it!`, false, { id: 'ember-kaliber5-test-support.pageObject' });
     let route = getUrl(page);
-    return visit(route);
+    await visit(route);
   }
 }
