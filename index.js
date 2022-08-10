eead9e52262747b6a0cd731ed9f994ebd60b0973
simonihmig <simon.ihmig@kaliber5.de>
2022-08-10 14:33:57 +0200

    Drop default Ember.Component test selector

--- index.js
+++ index.js
@@ -2,16 +2,4 @@
 
 module.exports = {
   name: 'ember-kaliber5-test-support',
-
-  included(app) {
-    let testSelectorPatch = 'vendor/add-default-test-selector.js';
-    app.import({
-      development: testSelectorPatch,
-      test: testSelectorPatch
-    });
-  },
-
-  isDevelopingAddon: function() {
-    return true;
-  }
 };
